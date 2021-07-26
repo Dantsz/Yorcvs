a5f7e9e247acf68d74ba011bce831df8fa095d63
Darkyun991 <costinasdan35@yahoo.com>
2021-07-26 19:21:22 +0300

    Added required dependencies to CmakeLists

--- src/Zepyt.cpp
+++ src/Zepyt.cpp
@@ -1,20 +1,69 @@
 
 #include <iostream>
 #include <SDL.h>
-
+#include <SDL_image.h>
 
 int main(int argc, char *argv[])
 {
  
-    // retutns zero on success else non-zero
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
-    }
-    SDL_Window* win = SDL_CreateWindow("GAME",
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
-    while (1);
- 
-    return 0;
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
+		std::cout << "Error SDL2 Initialization : " << SDL_GetError();
+		return 1;
+	}
+		
+	if (IMG_Init(IMG_INIT_PNG) == 0) {
+		std::cout << "Error SDL2_image Initialization";
+		return 2;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("First program", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	if (window == NULL) {
+		std::cout << "Error window creation";
+		return 3;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == NULL) {
+		std::cout << "Error renderer creation";
+		return 4;
+	}
+
+	SDL_Surface* lettuce_sur = IMG_Load("assets/lettuce.png");
+	if (lettuce_sur == NULL) {
+		std::cout << "Error loading image: " << IMG_GetError();
+		return 5;
+	}
+
+	SDL_Texture* lettuce_tex = SDL_CreateTextureFromSurface(renderer, lettuce_sur);
+	if (lettuce_tex == NULL) {
+		std::cout << "Error creating texture";
+		return 6;
+	}
+
+	SDL_FreeSurface(lettuce_sur);
+
+	while (true) {
+		SDL_Event e;
+		if (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				break;
+			}
+		}
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, lettuce_tex, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(lettuce_tex);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	IMG_Quit();
+	SDL_Quit();
+
+	return 0;
 }
+
+
+
+
