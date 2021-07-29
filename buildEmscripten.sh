b35334456081d8c2a82714a469b8abd3140703ef
Darkyun991 <costinasdan35@yahoo.com>
2021-07-29 21:32:18 +0300

    fixed emscripten script

--- buildEmscripten.sh
+++ buildEmscripten.sh
@@ -2,7 +2,7 @@ if [[ ! -d "build/Emscripten" ]]
 then 
     mkdir build/Emscripten
 fi
-g++ src/Yorcvs.cpp -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  -s USE_SDL_TTF=2  --embed-file assets  -s ALLOW_MEMORY_GROWTH=1 -o build/Emscripten/a.html
+em++ src/Yorcvs.cpp -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  -s USE_SDL_TTF=2  --embed-file assets  -s ALLOW_MEMORY_GROWTH=1 -o a.html
 
 
 echo "Done"
