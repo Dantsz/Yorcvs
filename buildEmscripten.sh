eed8b8d23e2507594e6cad2129a4c103a8a373e8
Darkyun991 <costinasdan35@yahoo.com>
2021-07-29 21:16:49 +0300

    lowercased M in mkdir

--- buildEmscripten.sh
+++ buildEmscripten.sh
@@ -1,6 +1,6 @@
 if [[ ! -d "build/Emscripten" ]]  
 then 
-    Mkdir build/Emscripten
+    mkdir build/Emscripten
 fi
 em++ src/Yorcvs.cpp -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  -s USE_SDL_TTF=2  --embed-file assets  -s ALLOW_MEMORY_GROWTH=1 -o build/Emscripten/a.html
 
