30468b849fa908589afe973d3542c6e64bd1a929
Darkyun991 <costinasdan35@yahoo.com>
2021-08-02 19:34:45 +0300

    Changed buildEmscripten command

--- buildEmscripten.sh
+++ buildEmscripten.sh
@@ -2,7 +2,7 @@ if [[ ! -d "build/Emscripten" ]]
 then 
     mkdir build/Emscripten
 fi
-em++ src/Yorcvs.cpp -std=c++20 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  -s USE_SDL_TTF=2  --embed-file assets  -s ALLOW_MEMORY_GROWTH=1 -o build/Emscripten/a.html
+em++ src/Yorcvs.cpp -std=c++2a -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  -s USE_SDL_TTF=2  --embed-file assets  -s ALLOW_MEMORY_GROWTH=1 -o build/Emscripten/a.html
 
 emrun build/Emscripten/a.html
 
