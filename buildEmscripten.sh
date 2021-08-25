9592b01b5f64ae406f183bd5a9054685ad1045e8
Darkyun991 <costinasdan35@yahoo.com>
2021-08-25 12:05:38 +0300

    updated cmakelists

--- buildEmscripten.sh
+++ buildEmscripten.sh
@@ -2,9 +2,9 @@ if [[ ! -d "build/Emscripten" ]]
 then 
     mkdir build/Emscripten
 fi
-em++ src/Yorcvs.cpp -std=c++2a -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  -s USE_SDL_TTF=2  --embed-file assets  -s ALLOW_MEMORY_GROWTH=1 -o build/Emscripten/a.html
+em++ src/Yorcvs.cpp -std=c++2a -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  -s USE_SDL_TTF=2  --embed-file assets  -s ALLOW_MEMORY_GROWTH=1 -o build/Emscripten/yorcvs.html
 
-emrun build/Emscripten/a.html
+emrun build/Emscripten/yorcvs.html
 
 echo "Done"
 sleep 10s
