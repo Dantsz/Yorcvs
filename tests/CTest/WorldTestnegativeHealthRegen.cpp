5d6f791b995f05edc0312f74a0bd9410107066ce
Dan Costinas <costinasdan35@yahoo.com>
2021-12-09 14:17:57 +0200

    Added duck test

---
+++ tests/CTest/WorldTestnegativeHealthRegen.cpp
@@ -0,0 +1,78 @@
+
+#include "../../src/Yorcvs.h"
+#include "../../src/common/utilities.h"
+
+#include <cassert>
+#include <iostream>
+
+constexpr size_t number_of_additional_bleeding_ducks = 10000;
+
+int main(int argc, char **argv)
+{
+    yorcvs::Timer timy{};
+    yorcvs::ECS world{};
+    timy.start();
+    yorcvs::Map map{"assets/map.tmx", &world};
+    std::cout << "Loading map took " <<  timy.get_ticks() << "ms" << '\n';
+    //deleting player
+    
+
+        //check if a player is on the map , it should be
+        assert(world.get_entities_with_component<playerMovementControlledComponent>() == 1);
+        //delete playerMovementControlledComponent
+        const size_t numberOfEntities = world.get_active_entities_number();
+        for(size_t i =  0 ; i < numberOfEntities ; i++)
+        {
+            if(world.has_components<playerMovementControlledComponent>(i))
+            {
+                world.remove_component<playerMovementControlledComponent>(i);
+            }
+        }
+        //no more players
+        assert(world.get_entities_with_component<playerMovementControlledComponent>() == 0 );
+        //add ducks(first create valid entity IDs  and test the performance) and then load ducks data into the entities
+         //create valid entities
+            const size_t  first_ad_duck_entity = world.get_active_entities_number();     
+            timy.start();  
+            for(size_t i = 0; i < number_of_additional_bleeding_ducks ; i++) 
+            {
+                const size_t k =   world.create_entity_ID();
+            }
+            std::cout << "Creating valid entities took "  << timy.get_ticks() <<  " ms" << '\n';
+            timy.start();
+
+            for(size_t i = 0 ; i < number_of_additional_bleeding_ducks ; i ++)
+            {
+                map.load_character_from_path(first_ad_duck_entity + i ,"assets/bleeding_duck.json");
+                //remove the collision
+                if(world.has_components<hitboxComponent>(first_ad_duck_entity + i))
+                {
+                    world.remove_component<hitboxComponent>(first_ad_duck_entity + i);
+                }
+            }
+
+            timy.stop();
+             std::cout<< "Loading the " + std::to_string(number_of_additional_bleeding_ducks) + " ducks took "  << timy.get_ticks() <<  " ms\n"; 
+            //verify they have been added
+            assert(world.get_active_entities_number() == numberOfEntities + number_of_additional_bleeding_ducks);
+
+        //update the world so the duck dissapear
+        float update_time = 0.0f;
+        float samples = 0.0f;
+        timy.start();
+        while(world.get_active_entities_number() != numberOfEntities - 1)
+        {
+            map.update(HealthSystem::update_time, {});
+            timy.stop();    
+            update_time += timy.get_ticks();
+            samples += 1.0f;
+            timy.start();
+        }
+       
+        timy.stop();
+        update_time += timy.get_ticks();
+        samples += 1.0f;        
+        std::cout<< "Updating the map " << samples << " times  took " << update_time << " (avg : " <<  update_time / samples << " )\n "; 
+        assert(world.get_active_entities_number() != numberOfEntities - 1);
+    return 0;
+}
