84c0b9635dbaa961c757fc222792480b4d375f86
Darkyun991 <costinasdan35@yahoo.com>
2022-06-26 17:05:19 +0300

    Separated utilities into different files

---
+++ src/common/utilities/log.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+namespace yorcvs
+{
+namespace MSGSEVERITY
+{
+[[maybe_unused]]constexpr size_t INFO = 0;
+[[maybe_unused]]constexpr size_t WARNING = 1;
+[[maybe_unused]]constexpr size_t ERROR = 2;
+} // namespace MSGSEVERITY
+
+/**
+ * @brief Simple function to print a message
+ *
+ * @param message - message to log
+ * @param level - level of severity
+ */
+template <typename T = std::string> void log(const T &message, size_t level = yorcvs::MSGSEVERITY::INFO) noexcept
+{
+    // TODO: std::format
+    std::cout << "Severity  " << level << "  Message: " << message << '\n';
+}
+
+}
---
+++ src/common/utilities/timer.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <chrono>
+namespace yorcvs
+{
+/**
+ * @brief Class for measuring time intervals
+ *
+ */
+class Timer
+{
+  public:
+    using time_type = std::chrono::time_point<std::chrono::steady_clock>;
+
+    /**
+     * @brief Sarts the timer, the timer is stopped by deault
+     *
+     */
+    void start()
+    {
+        mStarted = true;
+        mStopped = false;
+        stop_point = {};
+        begin_point = std::chrono::steady_clock::now();
+    }
+
+    /**
+     * @brief Stops the timer
+     *
+     */
+    void stop()
+    {
+        // Stop the timer
+        mStarted = false;
+        // Unpause the timer
+        mStopped = true;
+        stop_point = std::chrono::steady_clock::now();
+    }
+
+    /**
+     * @brief Checks if the timer has been stopped
+     *
+     * @return true the timer is paused
+     * @return false it's not paused
+     */
+    [[nodiscard]] bool is_paused() const
+    {
+        return mStopped;
+    }
+
+    /**
+     * @brief Checks if the timer is counting time
+     *
+     * @return true
+     * @return false
+     */
+    [[nodiscard]] bool is_started() const
+    {
+        return mStarted;
+    }
+
+    /**
+     * @brief Returns the time passed since timer was started
+     *
+     * @tparam return_type - what type should be returned
+     * @tparam cast_format - what unit of measure should be used(nanosecond,milisecond,second) , by default it's
+     * millisecond
+     */
+    template <typename return_type = size_t, typename cast_format = std::chrono::milliseconds>
+    [[nodiscard]] return_type get_ticks() const
+    {
+        if (mStopped)
+        {
+            return static_cast<return_type>(std::chrono::duration_cast<cast_format>(stop_point - begin_point).count());
+        }
+        return static_cast<return_type>(
+            std::chrono::duration_cast<cast_format>(std::chrono::steady_clock::now() - begin_point).count());
+    }
+
+    time_type begin_point = {};
+    time_type stop_point = {};
+
+    bool mStopped = false;
+    bool mStarted = false;
+};
+}
