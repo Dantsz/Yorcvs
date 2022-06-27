d8321ddddff0d34bd0b7a5af0000784a8f8545a9
Darkyun991 <costinasdan35@yahoo.com>
2022-06-28 00:38:31 +0300

    Reduced complexity of player control update function

--- src/game/systems/playercontrol.h
+++ src/game/systems/playercontrol.h
@@ -9,8 +9,6 @@
  */
 class PlayerMovementControl {
 public:
-    static constexpr float sprint_multiplier = 1.5f;
-
     PlayerMovementControl(yorcvs::ECS* parent, yorcvs::sdl2_window* parent_window)
         : world(parent)
         , window(parent_window)
@@ -22,18 +20,19 @@ public:
 
     void updateControls(const yorcvs::Vec2<float>& render_size, float dt)
     {
-
         cur_time += dt;
         for (const auto& ID : entityList->entitiesID) {
             if (controls_enable) {
-                dir = yorcvs::Vec2<float>(static_cast<float>(d_pressed) + static_cast<float>(a_pressed) * -1.0f,
-                    static_cast<float>(w_pressed) * -1.0f + static_cast<float>(s_pressed));
-                dir.normalize(); // now moving at 1000 units per second
-                dir *= player_default_speed; // move 30 units per second
+                const bool w_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_W);
+                const bool a_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_A);
+                const bool s_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_S);
+                const bool d_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_D);
+                const bool q_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_Q);
+                dir = compute_movement_direction(static_cast<float>(d_pressed), static_cast<float>(a_pressed), static_cast<float>(w_pressed), static_cast<float>(s_pressed));
                 if (q_pressed) {
                     if ((!world->has_components<staminaComponent>(ID))) {
                         dir *= PlayerMovementControl::sprint_multiplier;
-                    } else if ((world->has_components<staminaComponent>(ID) && world->get_component<staminaComponent>(ID).stamina - world->get_component<staminaComponent>(ID).stamina_regen > 0)) {
+                    } else if (world->get_component<staminaComponent>(ID).stamina - world->get_component<staminaComponent>(ID).stamina_regen > 0) {
                         dir *= PlayerMovementControl::sprint_multiplier;
                         if (cur_time >= update_time) {
                             world->get_component<staminaComponent>(ID).stamina -= 2 * world->get_component<staminaComponent>(ID).stamina_regen;
@@ -44,39 +43,38 @@ public:
                     cur_time = 0.0f;
                 }
                 world->get_component<velocityComponent>(ID).vel = dir;
-
-                w_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_W);
-                a_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_A);
-                s_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_S);
-                d_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_D);
-                q_pressed = window->is_key_pressed(yorcvs::Events::Key::YORCVS_KEY_Q);
-                if (a_pressed) {
-                    AnimationSystem::set_animation(world, ID, "walkingL");
-                } else if (d_pressed || s_pressed || w_pressed) {
-                    AnimationSystem::set_animation(world, ID, "walkingR");
-                } else if (world->get_component<velocityComponent>(ID).facing.x) {
-                    AnimationSystem::set_animation(world, ID, "idleL");
-                } else {
-                    AnimationSystem::set_animation(world, ID, "idleR");
-                }
+                AnimationSystem::set_animation(world, ID, select_facing_animation(a_pressed, d_pressed, w_pressed, s_pressed, world->get_component<velocityComponent>(ID).facing.x));
             }
             window->set_drawing_offset(world->get_component<positionComponent>(ID).position + dir - (render_size - world->get_component<spriteComponent>(ID).size) / 2);
         }
     }
-
+    static constexpr float sprint_multiplier = 1.5f;
     static constexpr float update_time = 1000.0f;
     static constexpr float player_default_speed = 0.033f;
-    float cur_time {};
-
     std::shared_ptr<yorcvs::EntitySystemList> entityList;
+    bool controls_enable = true;
+
+private:
+    static constexpr yorcvs::Vec2<float> compute_movement_direction(float move_right, float move_left, float move_up, float move_down)
+    {
+        // normalizing means moving at 1000 units per second
+        return yorcvs::Vec2<float>(move_right + move_left * -1.0f, move_up * -1.0f + move_down).normalize() * player_default_speed; // move 30 units per second
+    }
+    static const char* select_facing_animation(bool a_pressed, bool d_pressed, bool w_pressed, bool s_pressed, bool facing)
+    {
+        if (a_pressed) {
+            return "walkingL";
+        }
+        if (d_pressed || s_pressed || w_pressed) {
+            return "walkingR";
+        }
+        if (facing) {
+            return "idleL";
+        }
+        return "idleR";
+    }
+    float cur_time {};
     yorcvs::ECS* world;
     yorcvs::sdl2_window* window;
     yorcvs::Vec2<float> dir;
-
-    bool controls_enable = true;
-    bool w_pressed {};
-    bool a_pressed {};
-    bool s_pressed {};
-    bool d_pressed {};
-    bool q_pressed {};
 };
