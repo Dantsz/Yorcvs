ac141d5a6f540a4e9449c980d1f005379f714283
Darkyun991 <costinasdan35@yahoo.com>
2022-06-27 09:50:39 +0300

    added load character unit test

---
+++ tests/CTest/src/YorcvsLoadCharacter.cpp
@@ -0,0 +1,17 @@
+#include "../../../src/Yorcvs.h"
+#include "../../../src/common/utilities.h"
+#include <cassert>
+#include <iostream>
+int main(int argc, char** argv)
+{
+    yorcvs::Timer timy {};
+    yorcvs::ECS world {};
+    timy.start();
+    yorcvs::Map map { "assets/map.tmx", &world };
+    yorcvs::Entity entity { &world };
+    map.load_character_from_path(entity, "assets/duck.json");
+    const auto ok = world.has_components<identificationComponent, hitboxComponent, healthComponent, spriteComponent>(entity.id);
+    assert(ok);
+    map.load_character_from_path(entity, "file does not exist");
+    return 0;
+}
