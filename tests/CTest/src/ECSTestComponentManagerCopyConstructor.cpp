8ec0bcfc94682b740e1f6ccd772887766eabbf44
Dantsz <danscostinas@gmail.com>
2022-08-06 11:36:12 +0300

    Make test and yorcvs subdirectories and make them use the same dependecies

--- tests/CTest/src/ECSTestComponentManagerCopyConstructor.cpp
+++ tests/CTest/src/ECSTestComponentManagerCopyConstructor.cpp
@@ -1,9 +1,7 @@
-#include "../../../src/common/ecs.h"
+#include "common/ecs.h"
 #include <cassert>
 
-
-struct testC
-{
+struct testC {
     int x;
     int y;
 };
@@ -11,12 +9,12 @@ struct testC
 int main()
 {
     assert(1 == 1);
-    yorcvs::ComponentManager managerOriginal{};
-    yorcvs::ComponentManager managerNew{};
-   
+    yorcvs::ComponentManager managerOriginal {};
+    yorcvs::ComponentManager managerNew {};
+
     managerOriginal.register_component<testC>();
-    testC c{1,1};
-    managerOriginal.add_component<testC>(0,c);
+    testC c { 1, 1 };
+    managerOriginal.add_component<testC>(0, c);
     managerOriginal.get_component<testC>(0).x = 10;
     assert(managerOriginal.get_component<testC>(0).x == 10);
     managerNew = managerOriginal;
