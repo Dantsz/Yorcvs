3f6bffe0084c934dc1eb5d0f199d1eaf17a3a891
Dantsz <danscostinas@gmail.com>
2022-08-09 09:45:34 +0300

    Added animation picker

--- yorcvs/src/ui/animationeditor.h
+++ yorcvs/src/ui/animationeditor.h
@@ -1,25 +1,27 @@
 #pragma once
 #include "../common/ecs.h"
+#include "../game/components.h"
+#include "../game/systems/animation.h"
 #include "imgui.h"
 namespace yorcvs::ui {
 
 void show_current_animator_selector([[maybe_unused]] yorcvs::ECS* appECS, [[maybe_unused]] size_t ID)
 {
-    // const animationComponent& anim_comp = appECS->get_component<animationComponent>(ID);
-    // static std::string current_item {};
-    //     if (ImGui::BeginCombo("Animation", anim_comp.cur_animation.c_str())) {
-    //         for (const auto& [animation_name, animation] : anim_comp.animations) {
-    //             bool is_selected = (current_item == animation_name);
-    //             if (ImGui::Selectable(animation_name.c_str(), is_selected)) {
-    //                 current_item = animation_name;
-    //                 AnimationSystem::set_animation(appECS, ID, animation_name);
-    //             }
-    //             if (is_selected) {
-    //                 ImGui::SetItemDefaultFocus();
-    //             }
-    //         }
-    //         ImGui::EndCombo();
-    //     }
+    const animationComponent& anim_comp = appECS->get_component<animationComponent>(ID);
+    static std::string current_item {};
+    if (ImGui::BeginCombo("Animation", current_item.c_str())) {
+        for (const auto& [animation_name, animation] : anim_comp.animation_name_to_start_frame_index) {
+            bool is_selected = (current_item == animation_name);
+            if (ImGui::Selectable(animation_name.c_str(), is_selected)) {
+                current_item = animation_name;
+                AnimationSystem::set_animation_global(appECS, ID, animation_name);
+            }
+            if (is_selected) {
+                ImGui::SetItemDefaultFocus();
+            }
+        }
+        ImGui::EndCombo();
+    }
     ImGui::End();
 }
 
